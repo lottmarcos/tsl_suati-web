c18b4191aa81c80b95fa44f1d617296fd16bc829
Caio Lelis Penido <caiolelis337@hotmail.com>
2023-03-01 00:12:30 -0300

    login working

--- client/src/pages/admin/Admin.tsx
+++ client/src/pages/admin/Admin.tsx
@@ -1,5 +1,41 @@
+import { Field, Form, Formik } from "formik";
 import React from "react";
+import * as yup from "yup";
+
+import { SprintFormValues } from "./types";
 
 export const Admin = () => {
-  return <div>Admin</div>;
+  const sprintInitialValues: SprintFormValues = {
+    task: "",
+    deadline: "",
+    members: [],
+  };
+
+  const validationSprint = yup.object().shape({});
+
+  const handleSprint = (values: SprintFormValues) => {};
+
+  return (
+    <div>
+      <h1>Admin</h1>
+      <Formik
+        initialValues={sprintInitialValues}
+        onSubmit={(values) => handleSprint(values)}
+        validationSchema={validationSprint}
+      >
+        <Form>
+          <div>
+            <Field name="task" placeholder="Tarefa" />
+          </div>
+          <div>
+            <Field name="deadline" placeholder="Prazo" />
+          </div>
+          <div>
+            <Field name="members" placeholder="Membro(s)" />
+          </div>
+          <button>Adicionar</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
--- client/src/pages/member/Member.jsx
+++ client/src/pages/member/Member.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export const Member = () => {
-  return <div>Member</div>;
+  const [member, setMember] = useState({});
+
+  const params = useParams();
+  const id = params.id;
+
+  const fetchMember = (id) => {
+    axios
+      .post("http://localhost:8800/api/member/getData", {
+        id: id,
+      })
+      .then((res) => {
+        setMember(res.data);
+      });
+  };
+
+  useEffect(() => {
+    fetchMember(id);
+  }, [id]);
+
+  return (
+    <div>
+      <h1>Nome: {member?.name}</h1>
+      <h2>Subsistema: {member?.subsystemName}</h2>
+      <h2>Sistema: {member?.systemName}</h2>
+      <h2>Cargo: {member?.roleName}</h2>
+    </div>
+  );
 };
